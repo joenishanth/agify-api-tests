759e3e3fa13a0ab9b50e8b1aac131764792a87cb
Joe Nishanth <joenishanth@gmail.com>
2025-07-14 20:10:57 +1000

    - added AfterStep logging

--- hooks.ts
+++ hooks.ts
@@ -1,24 +1,28 @@
-import { BeforeAll, AfterAll, Before, After } from '@cucumber/cucumber'
+import { BeforeAll, AfterAll, Before, After, AfterStep } from '@cucumber/cucumber'
 
 
 BeforeAll(function () {
-  console.log('Running before all scenarios')
- 
+  console.log('*** Running before all scenarios ***\n')
+
 })
 
 Before(function ({pickle}) {
-  console.log(`Running before scenario: ${pickle.name}`)
+  console.log(`Running before scenario: ${pickle.name}\n`)
+})
+
+AfterStep(function ({pickleStep}) {
+  console.log(`Running after step in scenario: ${pickleStep.text}`)
 })
 
 After(function ({pickle, result }) {
-  console.log(`Running after scenario: ${pickle.name}`)
+  console.log(`Running after scenario: ${pickle.name}\t`)
   if (result && result.status === 'PASSED') {
-     console.log(`Scenario passed: ${pickle.name}`);
+     console.log(` - Scenario passed\n`);
   } else {
-   console.log(`Scenario failed: ${pickle.name}`);
+   console.log(` - Scenario failed`);
   }
 })  
 
 AfterAll(function () {
-  console.log('Running after all scenarios')
+  console.log('*** Running after all scenarios ***')
 })
